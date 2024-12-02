eab6b804c0fa548b69ebfa9606783d26b04bc100
bread <wgary283@gmail.com>
2024-12-01 22:41:01 -0600

    Edited world.php, index.html, world.css and world.js

--- world.js
+++ world.js
@@ -1,11 +1,36 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Page loaded... ");
     let lookupbtn = document.getElementById("lookup");
+    let lookupcitiesbtn = document.getElementById("lookupcities");
     lookupbtn.onclick = function() {
         console.log("Lookup function activated");
         let input = document.getElementById("country").value;
         console.log("input: " + input);
         fetch(`http://localhost/info2180-lab5/world.php?country=${encodeURIComponent(input)}`,{
+            method:"GET",
+            headers: {"Content-Type": "application/x-www-form-urlencoded"}
+        })
+        .then(response => {
+            if (response.ok) {
+                return response.text();
+            }
+            else {
+                return Promise.reject("An error has occured!")
+            }
+        })
+        .then(data => {
+            let result = document.getElementById("result");
+            result.innerHTML = data;
+        })
+        .catch(error => {
+            console.log("There was an error: " + error);
+        });
+    }
+    lookupcitiesbtn.onclick = function() {
+        console.log("Lookup Cities function activated");
+        let input = document.getElementById("country").value;
+        console.log("input: " + input);
+        fetch(`http://localhost/info2180-lab5/world.php?country=${encodeURIComponent(input)}&lookup=cities`,{
             method:"GET",
             headers: {"Content-Type": "application/x-www-form-urlencoded"}
         })
--- world.php
+++ world.php
@@ -35,24 +35,44 @@ if (isset($_GET["country"])) {
       echo "<h3>Country query empty???</h3>";
     }
     else {
-      $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$nuquery%';");
-      $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-      echo "<table>";
-      echo "<tr>";
-      echo "<th>Name</th>";
-      echo "<th>Continent</th>";
-      echo "<th>Independence Year</th>";
-      echo "<th>Head of State</th>";
-      echo "</tr>";
-      foreach ($results as $row):
+      if (isset($_GET["lookup"])) {
+        $stmt = $conn->query("SELECT countries.name AS countries_name, cities.name AS cities_name, cities.district, cities.population FROM cities INNER JOIN countries ON cities.country_code = countries.code WHERE countries.name LIKE '%$nuquery';");
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        echo "<table>";
         echo "<tr>";
-        echo "<td>" . $row['name'] . "</td>";
-        echo "<td>" . $row['continent'] . "</td>";
-        echo "<td>" . $row['independence_year'] . "</td>";
-        echo "<td>" . $row['head_of_state'] . "</td>";
+        echo "<th>Name</th>";
+        echo "<th>District</th>";
+        echo "<th>Population</th>";
         echo "</tr>";
-      endforeach;
-      echo "</table>";
+        foreach ($results as $row):
+          echo "<tr>";
+          echo "<td>" . $row['cities_name'] . "</td>";
+          echo "<td>" . $row['district'] . "</td>";
+          echo "<td>" . $row['population'] . "</td>";
+          echo "</tr>";
+        endforeach;
+        echo "</table>";
+      }
+      else {
+        $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$nuquery%';");
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        echo "<table>";
+        echo "<tr>";
+        echo "<th>Name</th>";
+        echo "<th>Continent</th>";
+        echo "<th>Independence Year</th>";
+        echo "<th>Head of State</th>";
+        echo "</tr>";
+        foreach ($results as $row):
+          echo "<tr>";
+          echo "<td>" . $row['name'] . "</td>";
+          echo "<td>" . $row['continent'] . "</td>";
+          echo "<td>" . $row['independence_year'] . "</td>";
+          echo "<td>" . $row['head_of_state'] . "</td>";
+          echo "</tr>";
+        endforeach;
+        echo "</table>";        
+      }
     }
   }
 }
