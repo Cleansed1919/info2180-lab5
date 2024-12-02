58243666ea2b1649414a424a34c27f24a47612d7
bread <wgary283@gmail.com>
2024-12-01 20:44:00 -0600

    Added world.js, edited world.css, edited world.php

---
+++ world.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Page loaded... ");
+    let lookupbtn = document.getElementById("lookup");
+    lookupbtn.onclick = function() {
+        console.log("Lookup function activated");
+        let input = document.getElementById("country").value;
+        console.log("input: " + input);
+        fetch(`http://localhost/info2180-lab5/world.php?country=${encodeURIComponent(input)}`,{
+            method:"GET",
+            headers: {"Content-Type": "application/x-www-form-urlencoded"}
+        })
+        .then(response => {
+            if (response.ok) {
+                return response.text();
+            }
+            else {
+                return Promise.reject("An error has occured!")
+            }
+        })
+        .then(data => {
+            let result = document.getElementById("result");
+            result.innerHTML = data;
+        })
+        .catch(error => {
+            console.log("There was an error: " + error);
+        });
+    }
+})
--- world.php
+++ world.php
@@ -1,7 +1,7 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
@@ -9,9 +9,36 @@ $stmt = $conn->query("SELECT * FROM countries");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+if (isset($_GET["country"])) {
+  $slimquery = trim($_GET["country"]);
+  $nuquery = filter_var($slimquery, FILTER_SANITIZE_STRING);
+  if (empty($nuquery) || $nuquery == "" || $nuquery == " ") {
+    echo "<ul>";
+    foreach ($results as $row): 
+      echo "<li>" . $row['name'] . " is ruled by" . $row['head_of_state'] . "</li>";
+    endforeach;
+    echo "</ul>";
+  }
+  else {
+    if (empty($nuquery)) {
+      echo "<h3>Country query empty???</h3>";
+    }
+    else {
+      $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$nuquery%';");
+      $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      echo "<ul>";
+      foreach ($results as $row): 
+        echo "<li>" . $row['name'] . " is ruled by" . $row['head_of_state'] . "</li>";
+      endforeach;
+      echo "</ul>";
+    }
+  }
+}
+else {
+  echo "<ul>";
+  foreach ($results as $row): 
+    echo "<li>" . $row['name'] . " is ruled by" . $row['head_of_state'] . "</li>";
+  endforeach;
+  echo "</ul>";
+}
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
